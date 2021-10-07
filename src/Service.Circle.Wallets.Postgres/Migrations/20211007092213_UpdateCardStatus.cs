c4de1b30f0015fc688c9c16d1384987674d8503b
Dmitrii Krasovskikh <dmitry.krasovskih@gmail.com>
2021-10-07 13:13:14 +0300

    - update db;

---
+++ src/Service.Circle.Wallets.Postgres/Migrations/20211007092213_UpdateCardStatus.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Service.Circle.Wallets.Postgres.Migrations
+{
+    public partial class UpdateCardStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
