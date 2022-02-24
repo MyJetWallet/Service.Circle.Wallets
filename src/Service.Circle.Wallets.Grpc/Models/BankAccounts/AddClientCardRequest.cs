c39ae305c4d0a69306a87aac4750ec6fe120a10a
Aleksandr Belousov <aleksandr.b@smplt.net>
2022-02-24 18:29:48 +0400

    fixed client

--- src/Service.Circle.Wallets.Grpc/Models/BankAccounts/AddClientCardRequest.cs
+++ src/Service.Circle.Wallets.Grpc/Models/BankAccounts/AddClientCardRequest.cs
@@ -13,18 +13,18 @@ namespace Service.Circle.Wallets.Grpc.Models.BankAccounts
         [DataMember(Order = 4)] public string AccountNumber { get; set; }
         [DataMember(Order = 5)] public string RoutingNumber { get; set; }
         [DataMember(Order = 6)] public string Iban { get; set; }
-        [DataMember(Order = 9)] public string BillingDetailsName { get; }
-        [DataMember(Order = 10)] public string BillingDetailsCity { get; }
-        [DataMember(Order = 11)] public string BillingDetailsCountry { get; }
-        [DataMember(Order = 12)] public string BillingDetailsLine1 { get; }
-        [DataMember(Order = 13)] public string BillingDetailsLine2 { get; }
-        [DataMember(Order = 14)] public string BillingDetailsDistrict { get; }
-        [DataMember(Order = 15)] public string BillingDetailsPostalCode { get; }
-        [DataMember(Order = 16)] public string BankAddressBankName { get; }
-        [DataMember(Order = 17)] public string BankAddressCity { get; }
-        [DataMember(Order = 18)] public string BankAddressCountry { get; }
-        [DataMember(Order = 19)] public string BankAddressLine1 { get; }
-        [DataMember(Order = 20)] public string BankAddressLine2 { get; }
-        [DataMember(Order = 21)] public string BankAddressDistrict { get; }
+        [DataMember(Order = 9)] public string BillingDetailsName { get; set; }
+        [DataMember(Order = 10)] public string BillingDetailsCity { get; set; }
+        [DataMember(Order = 11)] public string BillingDetailsCountry { get; set; }
+        [DataMember(Order = 12)] public string BillingDetailsLine1 { get; set; }
+        [DataMember(Order = 13)] public string BillingDetailsLine2 { get; set; }
+        [DataMember(Order = 14)] public string BillingDetailsDistrict { get; set; }
+        [DataMember(Order = 15)] public string BillingDetailsPostalCode { get; set; }
+        [DataMember(Order = 16)] public string BankAddressBankName { get; set; }
+        [DataMember(Order = 17)] public string BankAddressCity { get; set; }
+        [DataMember(Order = 18)] public string BankAddressCountry { get; set; }
+        [DataMember(Order = 19)] public string BankAddressLine1 { get; set; }
+        [DataMember(Order = 20)] public string BankAddressLine2 { get; set; }
+        [DataMember(Order = 21)] public string BankAddressDistrict { get; set; }
     }
 }
