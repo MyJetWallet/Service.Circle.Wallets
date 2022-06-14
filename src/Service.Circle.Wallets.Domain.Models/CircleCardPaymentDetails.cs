c3af64a5a75bff6ecee8830ab48094633ae13ebe
Alexey Novichikhin <alexey.n@smplt.net>
2022-06-14 18:20:45 +0300

    add FixFeeAmount in CircleCardPaymentDetails

--- src/Service.Circle.Wallets.Domain.Models/CircleCardPaymentDetails.cs
+++ src/Service.Circle.Wallets.Domain.Models/CircleCardPaymentDetails.cs
@@ -5,9 +5,12 @@ namespace Service.Circle.Wallets.Domain.Models
     [DataContract]
     public class CircleCardPaymentDetails
     {
+        // percentage part fee for the transaction
         [DataMember(Order = 1)] public decimal FeePercentage { get; set; }
         [DataMember(Order = 2)] public decimal MinAmount { get; set; }
         [DataMember(Order = 3)] public decimal MaxAmount { get; set; }
         [DataMember(Order = 4)] public string SettlementAsset { get; set; }
+        // fix part in circle fee for the transaction
+        [DataMember(Order = 5)] public decimal FixFeeAmount { get; set; }
     }
 }
